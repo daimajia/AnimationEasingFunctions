3c68ac85e77f7e880979f08c0c48b42366ed0de8
daimajia <daimajia@gmail.com>
2017-02-13 23:36:22 +0800

    remove com.nineoldandroids, now only support Android API 11+

--- library/src/main/java/com/daimajia/easing/Glider.java
+++ library/src/main/java/com/daimajia/easing/Glider.java
@@ -24,26 +24,27 @@
 
 package com.daimajia.easing;
 
-import com.nineoldandroids.animation.PropertyValuesHolder;
-import com.nineoldandroids.animation.ValueAnimator;
+
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 
 public class Glider {
 
-    public static ValueAnimator glide(Skill skill, float duration, ValueAnimator animator){
-        return Glider.glide(skill,duration,animator,null);
+    public static ValueAnimator glide(Skill skill, float duration, ValueAnimator animator) {
+        return Glider.glide(skill, duration, animator, (BaseEasingMethod.EasingListener[]) null);
     }
 
-    public static ValueAnimator glide(Skill skill, float duration, ValueAnimator animator, BaseEasingMethod.EasingListener ... listeners){
+    public static ValueAnimator glide(Skill skill, float duration, ValueAnimator animator, BaseEasingMethod.EasingListener... listeners) {
         BaseEasingMethod t = skill.getMethod(duration);
 
-        if(listeners!=null)
+        if (listeners != null)
             t.addEasingListeners(listeners);
 
         animator.setEvaluator(t);
         return animator;
     }
 
-    public static PropertyValuesHolder glide(Skill skill, float duration, PropertyValuesHolder propertyValuesHolder){
+    public static PropertyValuesHolder glide(Skill skill, float duration, PropertyValuesHolder propertyValuesHolder) {
         propertyValuesHolder.setEvaluator(skill.getMethod(duration));
         return propertyValuesHolder;
     }
